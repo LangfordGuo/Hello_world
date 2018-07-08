09fcfc6b8c3f6cae97daa06892942d4b4b8c3887
LangfordGuo <30494129+LangfordGuo@users.noreply.github.com>
2018-07-08 13:23:00 +0800

    Create 获取windowsIP地址作为字符串.bat

---
+++ 获取windowsIP地址作为字符串.bat
@@ -0,0 +1,6 @@
+REM 批处理学习，内容来源于互联网
+setlocal ENABLEEXTENSIONS & set "i=0.0.0.0" & set "j="
+for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
+  if not defined j for %%b in (%%a) do set "i=%%b" & set "j=1")
+endlocal & set "ip=%i%"
+echo IP 地址是：%ip%
